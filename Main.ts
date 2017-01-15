2e4317c088f15794664d8b178c45c3abe0e0c6ab
Jouza <vacekj@outlook.com>
2017-01-15 21:10:34 +0100

    Reorder Steps, add gaming test

--- Main.ts
+++ Main.ts
@@ -3,24 +3,30 @@
 var steps: Step[] = [];
 steps.push(
     new Step("start",
-        new Information("Hello", "Hope this displays correctly :)")));
+        new Information("Hello", "Welcome to Wizard")));
 steps.push(
-    new Step("misc_wifi",
-        new Checkbox("WiFi", "Do you want WiFi in your computer?", true)));
+    new Step("price",
+        new FormRange("Price", "How much should the computer cost AT MOST?")));
 steps.push(
     new Step("use",
         new Select("Use", "What are you going to use the Computer for?", [
             new Option("Gaming", "gaming"),
-            new Option("Office", "office")
-        ])));
+            new Option("Office", "office"),
+            new Option("Multimedia", "multimedia")
+        ]), [StepTag.Dynamic]));
 steps.push(
-    new Step("price",
-        new FormRange("Price", "How much should the computer cost AT MOST?"))
+    new Step("gaming_test",
+        new Information("DynAdd test - Gaming","DynAdd Test - Gaming"),
+        [StepTag.DynamicallyAdded])
 );
+steps.push(
+    new Step("misc_wifi",
+        new Checkbox("WiFi", "Do you want WiFi in your computer?", true)));
 steps.push(
     new Step("finish",
         new Information("Finished", "We are finished")));
-//StepHandler.loadSteps(LoadMethod.JSON, JSON.stringify(steps));
-StepHandler.Steps = steps;
+
+StepHandler.loadSteps(LoadMethod.Variable, steps);
+
 $(document)
     .ready(StepHandler.Init);
