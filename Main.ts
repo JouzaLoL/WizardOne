7660aa3e9341cfe55d48d8ae35dda4fbdfc0ed0c
Josef Vacek <Josef Vacek>
2017-02-08 18:35:48 +0100

    Rename default Steps to TestSteps

--- Main.ts
+++ Main.ts
@@ -1,34 +1,34 @@
 /// <reference path="StepHandler.ts" />
+
 // Manually load the steps for now
-var steps: Step[] = [];
-steps.push(
+var TestSteps: Step[] = [];
+TestSteps.push(
     new Step("start",
         new Information("Hello", "Welcome to Wizard")));
-steps.push(
+TestSteps.push(
     new Step("price",
         new FormRange("Price", "How much should the computer cost AT MOST?")));
-steps.push(
+TestSteps.push(
     new Step("use",
         new Select("Use", "What are you going to use the Computer for?", [
             new FormOption("Gaming", "gaming"),
             new FormOption("Office", "office"),
             new FormOption("Multimedia", "multimedia")
         ]), [StepTag.Dynamic]));
-steps.push(
+TestSteps.push(
     new Step("gaming_test",
-        new Information("DynAdd test - Gaming", "DynAdd Test - Gaming"),
-        [StepTag.DynamicallyAdded])
+        new Information("DynAdd test - Gaming", "DynAdd Test - Gaming"), [StepTag.DynamicallyAdded])
 );
-steps.push(
+TestSteps.push(
     new Step("misc_wifi",
         new Checkbox("WiFi", "Do you want WiFi in your computer?", true)));
-steps.push(
+TestSteps.push(
     new Step("finish",
         new Information("Finished", "We are finished")));
 
 
 // StepHandler.loadSteps(LoadMethod.Variable, steps);
-var encoded = Encoder.EncodeSteps(steps);
+var encoded = Encoder.EncodeSteps(TestSteps);
 StepHandler.loadSteps(LoadMethod.Local, encoded);
 
 $(document)
